0e89d686ae27159398f98bdc2fe5394e48ecc239
Дмитрий Резвых <rezvykh22@gmail.com>
2026-06-06 17:56:47 +0300

    added new rooms

--- rooms.lua
+++ rooms.lua
@@ -1,22 +1,46 @@
 local rooms = {
     entrance = {
+        id = 'entrance',
         name = "Вход в подземелье",
         description = "Темные каменные ступени ведут в холодную влажную тьму.",
     },
     hall = {
+        id = 'hall',
         name = "Большой зал",
         description = "Большой гулкий пустой зал слегка освещаемый свечами возле одной из стен",
     },
+    pit = {
+        id = 'pit',
+        name = "Яма",
+        description = "По краю яму спускается старая лестница. Она ведет в темные глубины",
+    },
+    pitBottom = {
+        id = 'pitBottom',
+        name = "Дно ямы",
+        description = "На дне ямы лежат скелеты бедняг которые здесь погибли",
+    },
     crypt = {
+        id = 'crypt',
         name = "Крипта",
         description = "Место упокоения, здесь в каменных гробах лежат тела монахов",
     }
 }
 
+--[[
+    entrance
+    |
+    hall - pit
+    |       |
+    crypt  pit-bottom
+]]-- 
+
 rooms.entrance.south = rooms.hall
 
 rooms.hall.north = rooms.entrance
 rooms.hall.south = rooms.crypt
+rooms.hall.east = rooms.pit
+
+rooms.pit.south = rooms.pitBottom
 
 rooms.crypt.north = rooms.hall
 
